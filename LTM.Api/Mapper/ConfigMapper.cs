a7553755250c2939eb353846e6d4f48fe38e1748
GemersonDenner <31903896+GemersonDenner@users.noreply.github.com>
2017-11-09 16:21:29 -0200

    Corrigindo caso do mapeamento
    Incluir registro de mapeamento de API.Produto>DAL.Produto

--- LTM.Api/Mapper/ConfigMapper.cs
+++ LTM.Api/Mapper/ConfigMapper.cs
@@ -13,6 +13,7 @@ namespace LTM.Api.Mapper
 		{
 			CreateMap<Entity.DAL.Usuario, Entity.API.LoginResponse>();
 			CreateMap<Entity.DAL.Produto, Entity.API.Produto>();
+			CreateMap<Entity.API.Produto,Entity.DAL.Produto>();
 			CreateMap<Entity.API.ProdutoAdicionar, Entity.DAL.Produto>();
 			CreateMap<Entity.API.UsuarioCriacao, Entity.DAL.Usuario>();
 			CreateMap<Entity.API.UsuarioAlteracao, Entity.DAL.Usuario>();
