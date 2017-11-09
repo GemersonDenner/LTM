2f2033887e678d78605eaaa748e3ecf568e059d6
GemersonDenner <31903896+GemersonDenner@users.noreply.github.com>
2017-11-08 22:02:32 -0200

    Métodos base API
    Subindo apenas a "carcaça" dos métodos da API

--- LTM.Api/Controllers/ProdutoController.cs
+++ LTM.Api/Controllers/ProdutoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using LTM.Entity.DAL;
 
 namespace LTM.Api.Controllers
 {
@@ -11,5 +12,33 @@ namespace LTM.Api.Controllers
     [Route("api/Produto")]
     public class ProdutoController : Controller
     {
-    }
+		[HttpGet("BuscarTodos/")]
+		public IEnumerable<Produto> BuscarTodos()
+		{
+			return null;
+		}
+
+		[HttpGet("BuscarTodos/{idProduto}")]
+		public Produto Buscar(Guid idProduto)
+		{
+			return null;
+		}
+
+		[HttpDelete("Deletar/{idProduto}")]
+		public void Deletar(Guid idProduto)
+		{
+
+		}
+
+		[HttpPost("Atualizar/")]
+		public void Atualizar([FromBody]Produto produto)
+		{
+
+		}
+		[HttpPost("Cadastrar/")]
+		public void Cadastrar([FromBody]Produto produto)
+		{
+
+		}
+	}
 }
--- LTM.Api/Controllers/UsuarioController.cs
+++ LTM.Api/Controllers/UsuarioController.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using LTM.Entity.DAL;
 
 namespace LTM.Api.Controllers
 {
-    [Produces("application/json")]
-    [Route("api/Usuario")]
-    public class UsuarioController : Controller
-    {
-    }
+	[Produces("application/json")]
+	[Route("api/Usuario")]
+	public class UsuarioController : Controller
+	{
+		[HttpGet("Buscar/{login}/{password}")]
+		public Usuario Buscar(string login, string password)
+		{
+			return null;
+		}
+
+		[HttpPost("Cadastrar/")]
+		public void Cadastrar([FromBody]Usuario usuario)
+		{
+
+		}
+
+		[HttpPost("Atualizar/")]
+		public void Atualizar([FromBody]Usuario usuario)
+		{
+
+		}
+	}
 }
